736dcf4d354446d024b7e0f7460e35e806876491
Daniylllll <143342450+Daniylllll@users.noreply.github.com>
2023-10-13 13:53:07 +0200

    Update all_work.py

--- all_work.py
+++ all_work.py
@@ -1,35 +1,24 @@
-import discord
 from aiogram import Dispatcher, Bot, types
 from config import *
 import disnake
-from discord.ext import commands as commands_ds
+from disnake.ext import commands as commands_ds
 from database import *
 
 bot = Bot(TG_TOKEN)
 dp = Dispatcher(bot=bot)
 
-# intents = discord.Intents.all()
-# bot_ds = commands_ds.Bot(command_prefix="/", intents=intents)
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user}')
-
-    async def send_ds_mess(self):
-        print(list(self.guilds))
-
-client = MyClient(intents=discord.Intents.all())
+intents = disnake.Intents.all()
+bot_ds = commands_ds.Bot(command_prefix="/", intents=intents)
 
 async def on_startup(_):
     await db_start()
 
+@bot_ds.event
+async def on_ready():
+    print(f'Logged in as {bot_ds.user.name}')
 
-# @bot_ds.event
-# async def on_ready():
-#     print(f'Logged in as {bot_ds.user.name}')
-
-# async def send_ds_mess():
-#     print(list(bot_ds.guilds))
+async def send_ds_mess():
+    print(list(bot_ds.guilds))
     # mess_info = await get_mess(0)
     # print(mess_info)
     # for data in mess_info:
@@ -46,19 +35,19 @@ async def on_startup(_):
 async def cd_start(message: types.Message):
     await message.answer("Привет друг!")
     text_for_ds = "Hello from Telegram!!!"
-    await update_mess("Hello world!!!", 1160219846571733042)
-    await client.send_ds_mess()
+    await update_mess(text_for_ds, 1160219846571733042)
+    await send_ds_mess()
 
 async def send_mess_tg():
     await bot.send_message(chat_id=5580704087, text="I'm here!")
 
 
-# @bot_ds.command()
-# async def start(ctx):
-#     await ctx.send("hi!")
-#     await send_ds_mess()
-#     await send_mess_tg()
+@bot_ds.command()
+async def start(ctx):
+    await ctx.send("hi!")
+    await send_ds_mess()
+    await send_mess_tg()
 
 if __name__ == '__main__':
     from config import DS_TOKEN
-    client.run(DS_TOKEN)
+    bot_ds.run(DS_TOKEN)
